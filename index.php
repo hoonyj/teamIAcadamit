2781ac3e129a7ab0a03a3dd1bf7e497991eb4613
Jaehoon Jung <jung032111@gmail.com>
2017-03-20 00:18:45 +0900

    testing
    Will be done before friday

--- index.php
+++ index.php
@@ -1,14 +1,170 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>Page Title</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="blueberry.css" />
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+    <script src="jquery.blueberry.js"></script>
+	<script src="jquery.blueberry.js"></script>
+
+<script>
+	$(window).load(function() {
+		$('.blueberry').blueberry();
+	});
+</script>﻿
 </head>
+<style>
+
+
+</style>
 <body>
+    <div id="header">
+        <div id="top">
+            <a href="inindex.html" id="logo"></a>
+            <nav>
+                <a href="#" id="menu-icon"></a>
+                <ul>
+                    <li><a href="inindex.html">HOME</a></li>
+                    <li><a href="detail.html">DETAIL</a></li>
+                    <li><a href="extra.html">EXTRAS</a></li>
+                    <li><a href="contact.html">CONTACT</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div id="menu">
+            <ul>
+                <li><a href="inindex.html">NEW</a></li>
+                <li><a href="inindex.html">CLOTHES</a></li>
+                <li><a href="inindex.html">MERCH</a></li>
+                <li><a href="inindex.html">CUSTOMIZE</a></li>
+                <li><a href="inindex.html">TINDER</a></li>
 
-<h1>This is a Heading</h1>
-<p>This is a paragraph.</p>
+            </ul>
+        </div>
+    </div>
 
-<h2> jung loves jamie and Khelly loves Enrico </h2>
 
+	<div id="doc">
+		<div id="content">
+			<div class="blueberry">
+				<ul class="slides">
+					<li><img src="images/slide001.png" /></li>
+					<li><img src="images/slide002.png" /></li>
+					<li><img src="images/slide003.png" /></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<section>
+		<img src="images/ability.png"/>
+		<h1>Best Abilities !</h1>
+		<p>Specialities in modern web application development.</p>
+	</section>
+	<section>
+		<img src="images/language.png"/>
+		<h1>Multilingual !</h1>
+		<p>Able to speak and understand several languages such as, Korean, English and Japanese.
+		   Can communicate with different people in a different languages.</p>
+	</section>
+	<section>
+		<img src="images/newthinking.png"/>
+		<h1>New Thinking !</h1>
+		<p>Think of new ideas everyday to learn, to experience and to adventure.
+			Critical thinking with brainstorming like a cloud will make me
+			better person as in Web Developer</p>
+	</section>
+
+		<!--Explain about myself Abilities-->
+
+	<div class="clear"></div>
+	<article>
+		<img src="images/myselfmain.png"/>
+	</article>
+	<aside>
+		<h2>HARD WORKER</h2>
+		<ul class="myself">
+			<li><h3>Structurer in "Hyper Text Mark-up Language"</h3></li>
+			<li><h3>Designer in "Cascading Style Sheet"</h3></li>
+			<li><h3>Technician in "PHP"</h3></li>
+			<li><h3>Mechanician in "JAVASCRIPT"</h3></li>
+			<li><h3>Analyst in "MySQL"</h3></li>
+		</ul>
+	</aside>
+
+		<!--Multilingual-->
+
+	<div class="clear"></div>
+	<div class="test">
+		<section>
+			<h1>KOREAN</h1>
+			<img src="images/korean.png" />
+			<p>It is my first language. There is no explanation.
+			Absolutely understand K-Drama</p>
+		</section>
+		<section>
+			<h1>ENGLISH</h1>
+			<img src="images/english.png" />
+			<p>It is my second language. with english, there is no problem
+			to communicate with different people in different country</p>
+		</section>
+		<section>
+			<h1>JAPANESE</h1>
+			<img src="images/japanese.png" />
+			<p>It is my third language. I'm still learning to understand fluently.
+			Need more time to learn.</p>
+		</section>
+
+	<!--NEW THINKING-->
+
+	<div class="clear"></div>
+		<section>
+			<h1>BUSINESS</h1>
+			<img src="images/business.png" />
+			<p>Try to having an own business here in the Philippines.
+			Check it out what products are available.</p>
+		</section>
+		<section>
+			<h1>IDEA</h1>
+			<img src="images/idea.png" />
+			<p>Keep brainstorming the brain with full of awesome idea I have !
+			This might help you to think weird and fantastic !</p>
+		</section>
+		<section>
+			<h1>COMING SOON !</h1>
+			<img src="images/comingsoon.png" />
+			<p>The awesome speciality from myself will explode soon !</p>
+		</section>
+	</div>
+	<!--Smelly footer-->
+	<footer>
+		<section>
+			<h3><b>Hoony's Think</b></h3>
+			<p><b>09167737988</b><br><br>
+			324 iACADEMY Plaza Sen. Gil Puyat Avenue, Bel Air, Makati City 1234<br>
+			201501240@iacademy.edu.ph</p>
+
+
+		</section>
+		<section>
+			<h3>Connect With Me !</h3>
+			<ul class="social">
+				<li><a href="https://www.facebook.com/jungiscorny"><img src="images/facebook.png"></a></li>
+				<li><a href="https://www.instagram.com/corny_jung/"><img src="images/instagram.png"></a></li>
+				<li><a href="http://www.kakao.com/talk"><img src="images/kakao.png"></a></li>
+				<li><a href="https://line.me/ko/"><img src="images/line.png"></a></li>
+			</ul>
+		</section>
+		<section>
+			<img src="images/logo_large.png"/>
+		</section>
+	</footer>
+
+	<!--Smelly copyright-->
+	<footer class="second">
+		<p>@Copyright - Hoony's Think, 2017.</p>
+	</footer>
 </body>
 </html>
